f91d44e8e47ce5a9e67569d93289da1de9279c8a
Jovana Jovanovic <jovanajovanovic930@gmail.com>
2021-08-31 19:21:46 +0200

    home page done

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -25,7 +25,15 @@
             height: 500px;
             padding: top 100px;
         }
-
-        
+        img.slider-img
+        {
+            height:400px !important;
+        }
+        .custom-product{
+            height:600px;
+        }
+        .slider-text{
+            background color: #c0c0c029 !important;
+        }
     </style>
 </html>
